02022cdd1328a58d39e93e541443d1592b8caf7a
Cy Dixon <94246986+cylissama@users.noreply.github.com>
2025-11-02 15:47:56 -0600

    updates

--- aiproject/settings.py
+++ aiproject/settings.py
@@ -123,8 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
+
+# Optional: ensure Django knows where to look for additional static files
+STATICFILES_DIRS = [
+    BASE_DIR / "dashboard" / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
